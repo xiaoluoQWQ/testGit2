9be75907623dc6a39eae98088107a9bf1abb77ae
xiaoluo <xiaoluo@qq.com>
2022-08-18 14:33:48 +0800

    third commit

--- src/main/java/com/git/test1.java
+++ src/main/java/com/git/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("你好啊");
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
